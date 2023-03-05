3abee3cc4c2a2e588191d8dcc91c4c69cf62dac9
kuansiu <247962284@qq.com>
2023-03-05 21:33:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   AUTHOR: 'TcahCat', // 您的昵称 例如 tangly1024
   BIO: '一个苦逼的大学生🍚', // 作者简介
   LINK: 'https://tachcat.com', // 网站地址
-  KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: 'TcahCat, blog, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: 'players_ultras0y@icloud.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: '', // 你的微博个人主页
-  CONTACT_TWITTER: 'twitter.com', // 你的twitter个人主页
-  CONTACT_GITHUB: 'github.com', // 你的github个人主页 例如 https://github.com/tangly1024
+  CONTACT_TWITTER: '', // 你的twitter个人主页
+  CONTACT_GITHUB: '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: '', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
@@ -214,7 +214,7 @@ const BLOG = {
   AVATAR: '/avatar.png', // 作者头像，被notion中的ICON覆盖。若无ICON则取public目录下的avatar.png
   TITLE: process.env.NEXT_PUBLIC_TITLE || 'NotionNext BLOG', // 站点标题 ，被notion中的页面标题覆盖
   HOME_BANNER_IMAGE: './bg_image.jpg', // 首页背景大图, 会被notion中的封面图覆盖，若无封面图则会使用代码中的 /public/bg_image.jpg 文件
-  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || '这是一个私人管理的站点', // 站点描述，被notion中的页面描述覆盖
+  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || '', // 站点描述，被notion中的页面描述覆盖
 
   // 开发相关
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
